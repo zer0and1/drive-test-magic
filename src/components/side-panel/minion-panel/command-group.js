4d5e4ce00190e31d8c199dfc115635346c9fed11
nikolaytoplev <nikolaytoplev@gmail.com>
2020-12-30 21:12:19 -0800

    add command panel

--- src/components/side-panel/minion-panel/command-group.js
+++ src/components/side-panel/minion-panel/command-group.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import MinionGroupFactory from './minion-group';
 import { Gear } from 'components/common/icons';
 
@@ -9,8 +9,31 @@ function CommandGroupFactory(MinionGroup) {
 
     render() {
       return (
-        <MinionGroup groupIcon={Gear} label="Command" toggled={false}>
-          <div style={{height: '150px'}}></div>
+        <MinionGroup groupIcon={Gear} label="Command" toggled={true}>
+          <table style={{ tableLayout: 'fixed', width: '100%' }}>
+            <tbody>
+              <tr>
+                <td>Operation Mode:</td>
+                <td colSpan="2"></td>
+              </tr>
+              <tr>
+                <td>Sleep Intervel:</td>
+                <td colSpan="2"></td>
+              </tr>
+              <tr>
+                <td>Session ID:</td>
+                <td colSpan="2"></td>
+              </tr>
+              <tr>
+                <td>Command:</td>
+                <td colSpan="2"></td>
+              </tr>
+              <tr>
+                <td>Last ACK:</td>
+                <td colSpan="2"></td>
+              </tr>
+            </tbody>
+          </table>
         </MinionGroup>
       );
     }
