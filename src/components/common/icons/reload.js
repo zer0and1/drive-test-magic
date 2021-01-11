e00176f9dcb6eca70221d33a189f411e92cd2c4b
nikolaytoplev <nikolaytoplev@gmail.com>
2021-01-11 06:05:04 -0800

    add two asset files

---
+++ src/components/common/icons/reload.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Base from './base';
+
+export default class Reload extends Component {
+  static propTypes = {
+    height: PropTypes.string
+  };
+
+  static defaultProps = {
+    height: '16px',
+    predefinedClassName: 'data-ex-icons-reload'
+  };
+
+  render() {
+    return (
+      <Base {...this.props}>
+        <path d="M52.069 27.547V6.641l-7.019 7.019C40.734 9.329 34.776 6.641 28.175 6.641 14.974 6.641 4.312 17.333 4.312 30.534s10.662 23.893 23.863 23.893c11.125 0 20.444-7.616 23.087-17.92h-6.212c-2.464 6.959-9.065 11.947-16.875 11.947-9.901 0-17.92-8.019-17.92-17.92s8.019-17.92 17.92-17.92c4.943 0 9.378 2.061 12.619 5.301l-9.632 9.632H52.069z"/>
+      </Base>
+    );
+  }
+}
