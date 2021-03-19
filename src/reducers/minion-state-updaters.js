ed3ce413cffa3418c8abfecc58ae0dc20e5c4ab3
Mykola Chernobryvets <nikolaytoplev@gmail.com>
2021-03-19 04:49:30 -0800

    fix: apply ordering by name after lastupdate

--- src/reducers/minion-state-updaters.js
+++ src/reducers/minion-state-updaters.js
@@ -152,7 +152,7 @@ export function loadMinionsUpdater(state, { firstLoading }) {
   const query = GQL_GET_MINIONS(state.selectedMinions[0]?.name);
   const loadMinionTask = GRAPHQL_QUERY_TASK({ query, fetchPolicy: 'network-only' }).bimap(
     result => {
-      const minions = _.orderBy(result.data.signal_db_minions, ['lastupdate'], 'desc');
+      const minions = _.orderBy(result.data.signal_db_minions, ['lastupdate', 'name'], ['desc', 'asc']);
       const sample = result.data.signal_db_signal_samples?.[0];
       return loadMinionsSuccess(minions, sample);
     },
@@ -627,7 +627,7 @@ export function addMinionSuccessUpdater(state, {data}) {
     minions: _.orderBy([
       ...state.minions,
       data
-    ], ['lastupdate'], 'desc'),
+    ], ['lastupdate', 'name'], ['desc', 'asc']),
     isUnselectingAll: true
   }
 }
